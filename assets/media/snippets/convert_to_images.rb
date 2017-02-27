739e9ed4846e68a6ce0bcaf3e0b8b5181caa830b
Ethan Nguyen <thanhnx.605@gmail.com>
2017-02-27 17:41:39 +0700

    Refine convert_to_images ruby script

--- assets/media/snippets/convert_to_images.rb
+++ assets/media/snippets/convert_to_images.rb
@@ -5,23 +5,43 @@ Setup instructions:
 
   * Pygments: `sudo pip install Pygments`
   * Python image libraries: `sudo pip install pillow`
+  * Custom fonts:
+    
+    - Install any `.ttf` font into either "~/Library/Fonts/", '/Library/Fonts/' or '/System/Library/Fonts'
+    - Replace `CUSTOM_FONT_NAME` with font's file name (whitespaces included), in this case is `Hack-Regular.ttf` -> CUSTOM_FONT_NAME = 'Hack-Regular'
+    - Replace `CUSTOM_FONT_SIZE` with desired font size
 
 =end
 
-require 'pathname'
-require 'fileutils'
+require "pathname"
+require "fileutils"
 
-Dir['posts/*'].each {|dir_name|
-  puts "Scanning directory #{dir_name}"
+CUSTOM_FONT_NAME = "Hack-Regular"
+CUSTOM_FONT_SIZE = 15
 
-  image_dir = Pathname.new(dir_name).sub('posts/', 'images/')
-  FileUtils.mkdir_p(image_dir)
+def convert_snippet(snippet_file, image_file)
+  pathname = Pathname.new(snippet_file)
+  puts "\tPygmentizing snippet file #{pathname.basename}"
 
-  Dir[dir_name + '/*'].each {|snippet_file|
-    pathname = Pathname.new(snippet_file)
-    image_file = image_dir + pathname.basename.sub(pathname.extname, '.png')
+  `pygmentize -f img \
+    -P "font_name=#{CUSTOM_FONT_NAME}" \
+    -P "font_size=#{CUSTOM_FONT_SIZE}" \
+    -o #{image_file} #{snippet_file}`
+end
 
-    puts "\tPygmentizing snippet file #{pathname.basename}"
-    `pygmentize -o #{image_file} #{snippet_file}`
+def process_all
+  Dir["posts/*"].each {|dir_name|
+    puts "Scanning directory #{dir_name}"
+
+    image_dir = Pathname.new(dir_name).sub("posts/", "images/")
+    FileUtils.mkdir_p(image_dir)
+
+    Dir["#{dir_name}/*"].each {|snippet_file|
+      pathname = Pathname.new(snippet_file)
+      image_file = image_dir + pathname.basename.sub(pathname.extname, ".png")
+      convert_snippet(snippet_file, image_file)
+    }
   }
-}
+end
+
+process_all
